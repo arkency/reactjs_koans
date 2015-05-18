6bdbe632bb16daffa189af3c90ff8813736f78b9
Wiktor Mociun <voter101@gmail.com>
2015-05-18 08:27:03 +0200

    Exercise #3: First component's implementation (doesn't work without additional effort of course).
    It's exercise about state changing and automatic view updating. Let's show user's how to show and update state.

---
+++ exercises/es6/03-WhatsYourName.jsx
@@ -0,0 +1,30 @@
+var React = require("react");
+
+class WhatsYourName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "" };
+  }
+
+  onNameChange(event) {
+    this.setState({attribute: "Something!"});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello {this.state.name}</p>
+        <input type="text" name="name" onChange={this.onNameChange} />
+      </div>
+    )
+  }
+}
+
+// Notice some details here:
+//   1. `onChange` attribute isn't placed betwen `" "`, but `{ }` - we want to reference function,
+//      not string
+
+// ProTip: Always specify input's `name` attribute: React uses it to identify inputs on page.
+//         Not doing so, may cause some long time wasted debugging your program.
+
+export default WhatsYourName
