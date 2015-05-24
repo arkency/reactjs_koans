575a0e2f387af69e54842ed6a146a16bb8f8207d
Wiktor Mociun <voter101@gmail.com>
2015-05-24 21:23:25 +0200

    Exercise #4 - quiz. I did gather some crucial details and made quiz to improve memory on these new concepts.

---
+++ koans/es6/04-Quiz.jsx
@@ -0,0 +1,34 @@
+// Try to answer those questions without peeking into tests. Just look around other exercises code.
+
+var Answers = {
+  // Question #1: What is the name of the class that we extend to create components class?
+  // TIP: See any of the exercises
+  answer1: "Enter your answer here",
+  
+  // Question #2: Is JSX converted directly into JavaScript?
+  // TIP: See the first exercise.
+  answer2: null, // true/false?
+
+  // Question #3: What's the name of the method that every component needs to have defined?
+  answer3: "toString",
+
+  // Question #4: If I want to set `div` component's HTML class, what attribute should I declare in
+  //              JSX?
+  // TIP: See exercise #2.
+  answer4: "class",
+
+  // Question #5: Can component's properties after component's initialization?
+  // TIP: See exercise #3
+  answer5: null, // true/false?
+
+  // Question #6: What's the name of the method for changing the component's state?
+  // TIP: See exercise #3
+  answer6: 'stateSet',
+
+  // Question #7: Do you need to bind component's methods (except `render`) to make it possible
+  //              to change the state?
+  // TIP: See exercise #3
+  answer7: null // true/false?
+};
+
+export default Answers;
---
+++ test/es6/04-Quiz.js
@@ -0,0 +1,32 @@
+import assert  from 'assert';
+import Answers from '../../src/es6/04-Quiz.js';
+
+describe("04 - Quiz", () => {
+  it("Question #1", () => {
+    assert.equal(Answers.answer1, "React.Component", "Check the line that contains `extends` word.")
+  });
+
+  it("Question #2", () => {
+    assert.equal(Answers.answer2, true, "It's not HTML (as we said in exercises 1 and 2, so what is it?");
+  });
+
+  it("Question #3", () => {
+    assert.equal(Answers.answer3, 'render', "In every component we did create there was one method with HTML-like looking code (JSX). What was that method?");
+  });
+
+  it("question #4", () => {
+    assert.equal(Answers.answer4, 'className', "`class` is a reserved word in JavaScript. See exercise 2.");
+  });
+
+  it("Question #5", () => {
+    assert.equal(Answers.answer5, false, "See the comments in exercise #3");
+  });
+
+  it("Question #6", () => {
+    assert.equal(Answers.answer6, 'setState', "See the `onNameChange` method from exercise #3");
+  });
+
+  it("Question #7", () => {
+    assert.equal(Answers.answer7, true, "I'll make it easier for you: yes you have. Why? See exercise #3! It's very important.");
+  });
+});
