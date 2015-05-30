6532c4711a1efbaa6f278c080fb70b13d6d83905
Nick Ostrovsky <nick@firedev.com>
2015-05-30 21:52:24 +0700

    02-PartiesList: return is not a function, reindent

--- koans/es6/02-PartiesList.jsx
+++ koans/es6/02-PartiesList.jsx
@@ -7,17 +7,17 @@ var React = require("react");
 // TASK #2: Change `ul` HTML attribute `class` to 'parties-list'.
 
 class PartiesList extends React.Component {
-    // We can nest DOM elements delivered by React just like HTML elements. See how this list looks 
-    // like. Doesn't it look similar?
-    render() {
-      return(
-        <ul className="FILL ME">
-          <li>Party at Aperture Laboratories</li>
-        </ul>
-      );
-    }
-    // Think about it: Why is that `className` attribute not called just `class`?
-    // Hint: JSX isn't HTML.
+  // We can nest DOM elements delivered by React just like HTML elements.
+  // See how this list looks like. Doesn't it look similar?
+  render() {
+    return (
+      <ul className="FILL ME">
+        <li>Party at Aperture Laboratories</li>
+      </ul>
+    );
+  }
+  // Think about it: Why is that `className` attribute not called just `class`?
+  // Hint: JSX isn't HTML.
 }
 
 export default PartiesList;
