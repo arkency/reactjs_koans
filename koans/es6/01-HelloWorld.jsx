abcbe7ac3bcb4c1e367784ca6f82a5c9aa106989
Nick Ostrovsky <nick@firedev.com>
2015-05-30 21:51:43 +0700

    Reindented

--- koans/es6/01-HelloWorld.jsx
+++ koans/es6/01-HelloWorld.jsx
@@ -6,26 +6,26 @@ var React = require("react");
 // TASK: Render HTML span with "Hello World" text.
 
 class HelloWorld extends React.Component {
-    // All components *must* have `render` method defined.
-    //
-    // To define component's render method, we use syntax called JSX. As you
-    // can see it looks similar to HTML. You can use normal JavaScript too, but
-    // JSX is much more popular, so we will stick to it. JSX gets converted to
-    // JavaScript code. It is here just for readability purposes.
-    //
-    // Note: You can read about `render` syntax here:
-    // https://facebook.github.io/react/docs/displaying-data.html
-    //
-    // Warning! JSX is not HTML - in next lessons you will notice the differences.
-    //
-    // React delivers big set of standard HTML elements like `div`, `p`,
-    // `canvas` etc. Here you can see usage of `div` element.
+  // All components *must* have `render` method defined.
+  //
+  // To define component's render method, we use syntax called JSX. As you
+  // can see it looks similar to HTML. You can use normal JavaScript too, but
+  // JSX is much more popular, so we will stick to it. JSX gets converted to
+  // JavaScript code. It is here just for readability purposes.
+  //
+  // Note: You can read about `render` syntax here:
+  // https://facebook.github.io/react/docs/displaying-data.html
+  //
+  // Warning! JSX is not HTML - in next lessons you will notice the differences.
+  //
+  // React delivers big set of standard HTML elements like `div`, `p`,
+  // `canvas` etc. Here you can see usage of `div` element.
 
-    render() {
-        return (
-            <div>FILL ME IN!</div>
-        );
-    }
+  render() {
+    return (
+        <div>FILL ME IN!</div>
+    );
+  }
 }
 
 // Note:
