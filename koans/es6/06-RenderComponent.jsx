16bfcde801429d16eda382d1d5b8dc6ddbd55270
Wiktor Mociun <voter101@gmail.com>
2015-06-03 19:52:51 +0200

    Reformatting. More descriptive comments. Changed `createClass` syntax to ES6 classes. Gave some big hint on using `React.render`.

--- koans/es6/06-RenderComponent.jsx
+++ koans/es6/06-RenderComponent.jsx
@@ -1,22 +1,31 @@
 var React = require('react');
 
-// Congratulations for realization of the previous exercise. That was bigger example. So this one you can treat as a rest.
-// You have already learnt how to create React's Components and how they affect each other. This excercise will show you
-// how to render prepared components on view.  
+// Congratulations for completing your first React challange!M#
+// You have already learnt how to create React's Components and how they affect
+// each other. This excercise will show you how to render components on website.
 //
-// Task #1: Fill the `renderName` method. It should render ReactElement into the DOM.
+// Task #1: Fill the `renderName` method. It should render ReactElement
+//          into the DOM.
+class Name extends React.Component {
+  render: function() {
+    return (
+      <p>'My name is koan'</p>
+    );
+  }
+}
 
-// This is a dead simple React component. You have to render it.
-var Name = React.createClass({
-
-	render: function() {
-		return (
-			<p>'My name is koan'</p>
-		);
-	}
-
-});
-
-// Hint: You have to use `React.render()` method. This method takes two params `ReactElement' and DOM node.
+// Hint: You have to use `React.render(ReactElement element, DOMNode node)` method.
+//        As you can see, this method takes two parameters:
+//        `ReactElement` and DOM node.
+//
+//        Don't mistake `React.render` with `render` method in component class.
+//        They are completely different methods!
+//
+//        Notice that class `Name` has type `React.Component`.
+//        It's a ReactComponent, not an ReactElement! You need to create an
+//        element from component before putting it into DOM. One way of doing
+//        that is using `React.createElement(ReactComponent component)`. E.g.
+//
+//        `let element = React.createElement(Name);`
 
 export default function renderName() {}
--- test/es6/06-RenderComponent.js
+++ test/es6/06-RenderComponent.js
@@ -1,8 +1,8 @@
-import assert      from 'assert';
-import renderName  from '../../src/es6/06-RenderComponent.js';
-import jsdom       from 'jsdom';
-import React       from 'react/addons';
-import _           from 'lodash';
+import assert     from 'assert';
+import renderName from '../../src/es6/06-RenderComponent.js';
+import jsdom      from 'jsdom';
+import React      from 'react/addons';
+import _          from 'lodash';
 
 describe("06 - RenderComponent", () => {
 
@@ -13,8 +13,8 @@ describe("06 - RenderComponent", () => {
   describe("Task #1 - render Name component", () => {
     it('There should be rendered a <p> element with name', () => {
       var element = document.getElementById('name');
-      assert.notEqual(element, null, "There should be only one paragraph"); 
-      assert.equal(element.innerHTML, "My name is koan", "The paragraph should contain 'My name is koan'"); 
+      assert.notEqual(element, null, "There should be only one paragraph");
+      assert.equal(element.innerHTML, "My name is koan", "The paragraph should contain 'My name is koan'");
     });
   });
 });
