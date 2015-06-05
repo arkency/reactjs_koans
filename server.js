d427766f352f3e9fd02ece48d93c9f94dc81cb5a
rybex <tomek.rybka@gmail.com>
2015-06-05 13:52:42 +0200

    Express configuration

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+app.get('/app.js', function (req, res) {
+  res.redirect('//localhost:9090/build/app.js');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/build/index.html');
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + '/build/style.css');
+});
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    noInfo: true,
+    historyApiFallback: true
+  }).listen(9090, 'localhost', function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+var server = app.listen(8080, function () {
+  console.log('Listening at http://localhost:%s', server.address().port);
+});
