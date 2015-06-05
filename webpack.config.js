5cfcc974732f3e228247b316bf284de4415a6aeb
rybex <tomek.rybka@gmail.com>
2015-06-05 13:53:47 +0200

    Webpack config. We use it to minify all code into one file.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:9090',
+    'webpack/hot/only-dev-server',
+    './exercises/es6/main'
+  ],
+
+  output: {
+    path:       __dirname + '/build/',
+    filename:   'app.js',
+    publicPath: 'http://localhost:9090/build/'
+  },
+
+  module: {
+    loaders: [
+      { test: path.join(__dirname, 'exercises/es6'), loaders: ['react-hot', 'babel-loader']}
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
