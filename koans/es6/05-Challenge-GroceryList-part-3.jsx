5e6b07a8970f3d73bae3e8152462ef9e4863ba43
Wiktor Mociun <voter101@gmail.com>
2015-06-13 22:47:36 +0200

    Improve description of task #3

--- koans/es6/05-Challenge-GroceryList-part-3.jsx
+++ koans/es6/05-Challenge-GroceryList-part-3.jsx
@@ -1,29 +1,21 @@
 var React = require("react");
 
-// This exercise is a bit bigger than the previous ones.
-// We would make reactive grocery list.
-//
-//
 // Task: User would need to clear whole grocery list in one click.
-//          Render a proper button under your list and implement the `clearList` method.
-//          This method should clear `groceries` array placed in your state.
-//          This is similar to previous task so I don't want to say nothing more.
-//          Have fun.
+//       Render a proper button under your list and implement the `clearList` method.
+//       This method should clear `groceries` array placed in your state.
+//       This is similar to previous task so I don't want to say nothing more.
+//       Have fun.
 //
-//          Caution: Remember that you should change state only using `setState`
-//                   method. The only exception of that rule is state definition
-//                   in component's class constructor.
-//
+//       Caution: Remember that you should change state only using `setState`
+//                method. The only exception of that rule is state definition
+//                in component's class constructor.
 
 class GroceryList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       groceries: [
-        {
-          name: "Apples",
-          selected: false
-        }
+        { name: "Apples" }
       ],
       newGroceryName: ""
     };
@@ -33,35 +25,31 @@ class GroceryList extends React.Component {
     this.inputChanged = this.inputChanged.bind(this);
   }
 
-  // Warning: You shouldn't change this method except working with extra tasks.
   inputChanged(event) {
-    this.setState({newGroceryName: event.target.value});
+    this.setState({ newGroceryName: event.target.value });
   }
 
   addGroceryItem() {
-    if(this.state.newGroceryName != "") {
-      this.setState({groceries: this.state.groceries.concat([{name: this.state.newGroceryName}])});
+    if(this.state.newGroceryName) {
+      let newGroceryItem = { name: this.state.newGroceryName };
+      this.setState({
+        groceries: this.state.groceries.concat([newGroceryItem])
+      });
     }
   }
 
   // Fill the definition of the following method to allow clearing the list
   // Hint: You can just simply set the groceries to an empty array.
-  clearList() {}
+  clearList() {
+    // Put your code here
+  }
 
   render() {
-    // As you can see we prepared all view elements for you. Please add necesary handlers and
-    // render all elements.
     let groceriesComponents = [],
         newProductInput,
         newProductAddButton,
         clearListButton;
 
-    // Properties are a way to pass parameters to your React components.
-    // We mentioned this in the third exercise. Properties are to React
-    // components what attributes are to HTML elements.
-    //
-    // This is a method to pass a configuration to child components.
-    // As you can see here we have defined `grocery` property for each `GroceryListItem`.
     for(var index = 0; index < this.state.groceries.length; index++) {
       groceriesComponents.push(
           <GroceryListItem
@@ -71,16 +59,14 @@ class GroceryList extends React.Component {
     }
 
     newProductInput = <input className='new-item' type="text" onChange={this.inputChanged}/>;
-
     newProductAddButton = <button className='add-product' onClick={this.addGroceryItem}>Add new Product</button>;
 
-    //Something is missing here... You will need this component to finish task #3
-    clearListButton = <button className='clear-list'>Clear the List</button>;
+    clearListButton = <button onClick={this.clearList} className='clear-list'>Clear the List</button>;
 
     return (
       <div>
         <ul>
-        {groceriesComponents}
+          {groceriesComponents}
         </ul>
         {newProductInput}
         {newProductAddButton}
