05f22c9b38a63324be05435ee42abf3e6b87b5b8
Wiktor Mociun <voter101@gmail.com>
2015-07-07 12:49:40 +0200

    Closes #30. ES6 imports are hoisted. Let's switch to explicit `require` in this file

--- test/es6/helpers.js
+++ test/es6/helpers.js
@@ -1,12 +1,12 @@
-import assert from 'assert';
-import jsdom  from 'jsdom';
+let assert = require('assert');
+let jsdom  = require('jsdom');
 
 global.document  = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
 global.window    = document.parentWindow;
 global.navigator = { userAgent: "Node.JS" };
 
-import React  from 'react/addons';
-import _      from 'lodash';
+let React = require('react/addons');
+let _     = require('lodash');
 
 global.React     = React;
 global.assert    = assert;
