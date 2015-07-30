75acc9fa8e5ecc85dac105bf3640866a6959f81d
Myles Borins <myles@famo.us>
2015-07-30 13:31:53 -0700

    Make lesson 3 a bit less gendered

--- test/es6/03-WhatsYourName.js
+++ test/es6/03-WhatsYourName.js
@@ -41,7 +41,7 @@ describe("03 - What's Your Name?", () => {
     });
 
     describe("Task #2 - don't greet user if name wasn't provided", () => {
-      it("Should display welcoming message if user didn't provide his name", () => {
+      it("Should display welcoming message if user didn't provide their name", () => {
         var input     = _.first(inputs);
         var paragraph = _.first(paragraphs);
 
