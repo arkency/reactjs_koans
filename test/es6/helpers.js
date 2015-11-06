d7b7a452cb084b79bb9684e453dd95751d20566d
Wiktor Mociun <voter101@gmail.com>
2015-11-06 10:27:05 +0100

    Fix JSDOM initialization for the new version

--- test/es6/helpers.js
+++ test/es6/helpers.js
@@ -1,8 +1,10 @@
 let assert = require('assert');
 let jsdom  = require('jsdom');
 
-global.document  = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
-global.window    = document.parentWindow;
+global.document  = jsdom.jsdom(undefined, {
+  virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+});
+global.window    = global.document.defaultView;
 global.navigator = { userAgent: "Node.JS" };
 
 let React = require('react/addons');
